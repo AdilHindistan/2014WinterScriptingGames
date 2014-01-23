ec8ba7ad40678f1adb37ef5528bc1b731f094321
Sunny Chakraborty <sunnyc7@gmail.com>
2014-01-23 18:33:02 -0500

    Sunny's stack add

--- WSG2014_01_Pairs/sunny/pairin-with-ij.ps1
+++ WSG2014_01_Pairs/sunny/pairin-with-ij.ps1
@@ -30,4 +30,51 @@ Write-Output "$itm, Count: $count"
 $count++
 }
 
-$(Get-Date -Format MM/dd/yyyy-[HH:MM:ss])
+#ignore
+
+#Main combinaiton Matrix of Pairs
+$horiz= $names.Count
+$outputArr = New-Object 'object[,]' $horiz,$horiz
+for ($i=0; $i -lt $horiz; $i++) {
+       for ($j=0; $j -lt $horiz; $j++) {
+        $outputArr[$i,$j] ="$($names[$i]),$($names[$j])"
+        }
+}
+$outputArr
+#Delete main matrix with IgnoreList #Fitness Test
+
+#How about dealing with a shorter List
+$final = @()
+foreach ($user in $names) {
+    if($primary -notcontains $user) {
+    $final +=$user
+    }
+}
+$final.Count
+
+[string[]]$names = get-content C:\Github\PhillyPosh\WSG2014_01_Pairs\sunny\names.txt
+[string[]]$primary= get-content C:\Github\PhillyPosh\WSG2014_01_Pairs\sunny\primary.txt
+
+$myStack = new-object  System.Collections.Stack  
+foreach ($item in $final) {
+$myStack.Push($item)
+}
+
+        
+$ncount = $names.Count
+$pcount = $primary.Count
+$resultArr= New-Object 'object[,]' $ncount,$ncount  
+for ($i=0; $i -lt $pcount; $i++) {
+       for ($j=0; $j -lt $ncount; $j++) {
+        $temp = $myStack.Peek()
+        $resultArr[$i,$j] ="$($primary[$i]),$($names[$j])"
+        }
+}
+
+$names = Get-Content .\names.txt
+$myStack = new-object  System.Collections.Stack  
+foreach ($item in $names) {
+$myStack.Push($item)
+}
+#need to use Peek, to inspect the element being popped out is -Contains Primary
+$myStack.Peek()
