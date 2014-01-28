6e5c6bcfe473c16550da5f6547fb4367935c5cb9
Sunny Chakraborty <sunnyc7@gmail.com>
2014-01-28 16:19:37 -0500

    PortScanner Sysint Downloader

---
+++ WSG2014_02_Security-Footprint/ExecAutorunsFromWeb.ps1
@@ -0,0 +1,6 @@
+﻿$wc = new-object System.Net.WebClient
+$uri = 'http://live.sysinternals.com/procexp.exe'
+#$tmpProcExp = "$env:TEMP\procexp.exe"
+$tmpAuto = "$env:TEMP\autorunsc.exe"
+$wc.DownloadFile($uri,$tmpAuto)
+
