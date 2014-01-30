fbac74be89123a64f0813e7384d196446a66872e
gir960 <gantcho@gmail.com>
2014-01-29 22:02:49 -0500

    check script

---
+++ WSG2014_02_Security-Footprint/prod/chec-script.ps1
@@ -0,0 +1,46 @@
+﻿
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function check-script
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $Scriptfile
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+    switch ($(Get-AuthenticodeSignature $Scriptfile|select -ExpandProperty status)) 
+    {
+    "notsigned"{Write-Warning "skipping not signed"
+    return $false
+    }
+    "valid" {Write-Verbose "script validated"
+    return $true}
+    "notvalid" {Write-Warning "cannot validate certificate. Skipping execution"
+    return $false
+    }
+    }
+    }
+    End
+    {
+    }
+}
