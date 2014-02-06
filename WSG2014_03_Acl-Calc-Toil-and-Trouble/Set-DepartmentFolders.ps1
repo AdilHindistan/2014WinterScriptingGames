aa948e94eda8f2442dd2360270d3061e5e5da25a
Adil Hindistan <adil.hindistan@gmail.com>
2014-02-06 17:04:08 -0500

    Create a nested group structure
    Create a nested group structure to analyze/work with AD permissions

--- WSG2014_03_Acl-Calc-Toil-and-Trouble/Set-DepartmentFolders.ps1
+++ WSG2014_03_Acl-Calc-Toil-and-Trouble/Set-DepartmentFolders.ps1
@@ -58,6 +58,74 @@ Function New-DepartmentFolder {
     #endregion team_folders
 }
 
+Function Set-TemplateGroups {
+<#
+    .SYNOPSIS
+    Setup AD group for department
+    .DESCRIPTION
+    Sets up AD groups for department, teams, and team leads
+    .EXAMPLE
+        PS> Set-TemplateGroups -Verbose
+        VERBOSE: Attempting to create department group grp_Finance
+        VERBOSE: Attempting to create team group grp_Receipts
+        VERBOSE: Adding grp_Receipts into grp_Finance as a member
+        VERBOSE: Attempting to create team lead group grp_Receipts_Lead
+        VERBOSE: Adding grp_Receipts_Lead into grp_Receipts
+        VERBOSE: Attempting to create team group grp_Payments
+        VERBOSE: Adding grp_Payments into grp_Finance as a member
+        VERBOSE: Attempting to create team lead group grp_Payments_Lead
+        VERBOSE: Adding grp_Payments_Lead into grp_Payments
+        VERBOSE: Attempting to create team group grp_Audit
+        VERBOSE: Adding grp_Audit into grp_Finance as a member
+        VERBOSE: Attempting to create team lead group grp_Audit_Lead
+        VERBOSE: Adding grp_Audit_Lead into grp_Audit
+        VERBOSE: Attempting to create team group grp_Accounting
+        VERBOSE: Adding grp_Accounting into grp_Finance as a member
+        VERBOSE: Attempting to create team lead group grp_Accounting_Lead
+        VERBOSE: Adding grp_Accounting_Lead into grp_Accounting
+#>
+    [CMDLETBINDING()]
+    param (            
+            [string]$department='Finance',
+            [string[]]$team=@('Receipts','Payments','Audit','Accounting'),
+            [string]$ADPath = "OU=groups,OU=EUDE,DC=nyumc,DC=org"    ## !!!REMOVE_THIS_BEFORE_SUBMISSION!!!
+            )    
+    Begin {
+             try {
+                Write-Verbose "Attempting to create department group grp_$department"            
+                $departmentGroup = New-AdGroup -Name "grp_${department}" -GroupScope DomainLocal -GroupCategory Security -Path $ADPath -Server ADSWCDCPVM008 -PassThru 
+                #$departmentGroup = Get-ADGroup -Identity "grp_${department}"
+            }
+            catch {
+                Write-Verbose $_
+            }       
+    
+    }
+    Process {
+
+        Foreach ($t in $team) {
+            try {
+                Write-Verbose "Attempting to create team group grp_${t}"            
+                $TeamGroup = New-AdGroup -Name "grp_${t}" -GroupScope DomainLocal -GroupCategory Security -Path $ADPath -Server ADSWCDCPVM008 -PassThru 
+                
+                Write-Verbose "Adding grp_${t} into grp_$department as a member"
+                Add-ADGroupMember -Identity $departmentGroup -Members $TeamGroup -Server ADSWCDCPVM008
+
+                Write-Verbose "Attempting to create team lead group grp_${t}_Lead"
+                $TeamLeadGroup = New-AdGroup -Name "grp_${t}_Lead" -GroupScope DomainLocal -GroupCategory Security -Path $ADPath -Server ADSWCDCPVM008 -PassThru 
+                
+                Write-Verbose "Adding grp_${t}_Lead into grp_${t}"
+                Add-ADGroupMember -Identity $TeamGroup -Members $TeamLeadGroup -Server ADSWCDCPVM008
+
+            }
+            catch {
+                Write-Verbose $_
+            }
+        }
+    
+    }        
+}
+
 Function New-DepartmentACL {
 <#
     .SYNOPSIS
@@ -67,8 +135,6 @@ Function New-DepartmentACL {
 Param(
         [string]$department='Finance'
     )
-
-
 }
 
 #endregion functions
@@ -76,5 +142,6 @@ Param(
 #region Main_Script
 
 New-DepartmentFolder
+Set-TemplateGroups
 
 #endregion Main_Script
