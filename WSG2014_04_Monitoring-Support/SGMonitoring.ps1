7f26f05dd2ec3123745ef682920555ad2c1f488c
dafuni <ifiok@mdafuni.com>
2014-02-09 10:11:43 -0500

    adding started DSC file

---
+++ WSG2014_04_Monitoring-Support/SGMonitoring.ps1
@@ -0,0 +1,36 @@
+﻿Configuration SGEvent4
+{
+    
+    param ( $NodeName )
+
+    Node $NodeName
+    
+    {
+        
+        Registry DRSmonitoing
+        {
+            Ensure = "Present" 
+            Key = "HKLM:\SOFTWARE\DRSmonitoing"
+            ValueName = "Monitoring"
+            ValueData = "1"
+        }
+
+        File DirectoryCopy
+        {
+            Ensure = "Present" 
+            Type = "file" 
+           # Recurse = $true 
+            SourcePath = "C:\SG2014\monitoring.xml"
+            DestinationPath = "c:\drsmonitoring\monitoring.xml"    
+        }
+
+        #trying to use the Log to create the report
+
+        #Log AfterDirectoryCopy
+        #{
+        #    # The message below gets written to the Microsoft-Windows-Desired State Configuration/Analytic log
+        #    Message = "Finished running the file resource with ID DirectoryCopy"
+        #    DependsOn = "[File]DirectoryCopy" # This means run "DirectoryCopy" first.
+        #}
+    }
+}
