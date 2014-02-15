96479fe26fef1ad772eb6bfd430883357abd3cb8
Adil Hindistan <adil.hindistan@gmail.com>
2014-02-15 14:18:20 -0500

    Updates
    Rewrote copy config & updated XML creation

--- WSG2014_04_Monitoring-Support/SG-Monitoring.ps1
+++ WSG2014_04_Monitoring-Support/SG-Monitoring.ps1
@@ -26,38 +26,72 @@ InputFile
 [CmdletBinding(SupportsShouldProcess)]
 Param
 (
-    # Full path to the csv config file
+    # Full path to the csv config file    
     [Parameter(HelpMessage="Enter full path to the config csv file")]
     [ValidateNotNullOrEmpty()]
     [ValidateScript({(test-path $_) -and ((get-item $_).extension -eq '.csv')})]    
     [string]$InputFile=".\servers.csv"
     
 )
-$csv = Import-Csv $InputFile
+
 
 #region Helper Functions
 
-Function Create-DRSMonitoringFile {
+Function ConvertFrom-CSVToXMLMonitoringFile {
+<#
+    .SYNOPSIS
+    Convert CSV Input file path to XML File
+    
+    .DESCRIPTION
+    Takes in a CSV File path containing Server Monitoring Data, and convert it to an xml configuration file for each server
 
-param($csvfile)
+#>
+[CMDLETBinding()]
+    param(        
+        [PSObject]$Servers
+        )
 
-Begin {
-    $csv = Import-Csv $csvfile
-    $xmlpath = "$pwd\drsconfig.xml"
-    $xmldata = New-Object XML
-    $xmldata.Load($xmlpath)
-    }
-Process {
-    foreach ($item in $csv){
-        $xmldata.DRSmonitoring.Server.Name = $item.Server
-        $xmldata.DRSmonitoring.Server.IPAddress = $item.IP
-        $xmldata.DRSmonitoring.Monitoring.MonitorCPU =$item.CPU
-        $xmldata.DRSmonitoring.Monitoring.MonitorRAM =$item.RAM
-        $xmldata.DRSmonitoring.Monitoring.MonitorDisk=$item.Disk
-        $xmldata.DRSmonitoring.Monitoring.MonitorNetwork=$item.Network
-        $xmldata.save("$pwd\$($item.Server)-DRSMonitoring.xml")
+    Begin {
+            $msgSource = $MyInvocation.MyCommand.Name               
+
+            &$log "Loading template XML into memory - $PSScriptRoot\drsconfig.xml"
+            $xmlpath = "$PSScriptRoot\drsconfig.xml"
+
+            $xmldata = New-Object System.Xml.XmlDocument
+            $xmldata.Load($xmlpath)
+
+            if (-not (Test-Path C:\MonitoringFiles)) {
+                try {                
+                        New-Item -Path C:\MonitoringFiles -ItemType Directory |Out-Null
+                        &$log "Local Storage folder does not exist. Creating C:\MonitoringFiles"
+                    }
+                catch {
+                        &$log "Failed to create local storage. Script will exit. $_"
+                        exit 1   
+                }
+            }
+
         }
-    }
+    Process {
+            foreach ($item in $Servers){
+                    $xmldata.DRSmonitoring.Server.Name = $item.Server
+                    $xmldata.DRSmonitoring.Server.IPAddress = $item.IP
+                    $xmldata.DRSmonitoring.Monitoring.MonitorCPU =$item.CPU
+                    $xmldata.DRSmonitoring.Monitoring.MonitorRAM =$item.RAM
+                    $xmldata.DRSmonitoring.Monitoring.MonitorDisk=$item.Disk
+                    $xmldata.DRSmonitoring.Monitoring.MonitorNetwork=$item.Network
+
+                    
+                    try {
+                            $xmldata.save("C:\MonitoringFiles\$($item.Server)-DRSMonitoring.xml")
+                            &$log "Saved monitoring config file C:\MonitoringFiles\$($item.Server)-DRSMonitoring.xml"
+                        }
+                    catch {
+                            Write-Error "Failed trying to save config file to c:\MonitoringFiles. Script will exit. $_"
+                            exit 1                              
+                    }
+             }
+        }
 } #End of Function Create-DRSMonitoringFile
 
 Function Get-RegistryHive {
@@ -92,55 +126,106 @@ $regvalue = 1
         }
 } #End of Function Set-RegistryHive
 
-Function Create-Path {
-param(
-$computer,
-$hive = 'SOFTWARE\DRSMonitoring',
-$regvalue = 1
-)
+Function Copy-ConfigFileToServer {
+<#
+    .SYNOPSIS
+    Copies Monitoring Config file to servers
 
-process {    
-    $temppath = "\\$computer\c$\DRSMonitoring"
-       
-    #Check if TEMP Exists.
-    if(!(Test-Path -Path $temppath)) {
-   
-        try {
-            #Else Create Temp Folder
-            $process = ([wmiclass]"\\$computer\root\cimv2:win32_Process")
-            $command = 'cmd /c mkdir C:\DRSMonitoring'
-            $process.create($command)
-            }
-    catch [Exception] {
-        "DRS Monitoring FILE CREATION ERROR: $computer $_.Exception.Message"
-        }
-    } #end of IF
-    } #end of Process Block
-} #End of Function Create-Path
-
-Function Copy-DRSFile{
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$ComputerName,
-    [string]$ConfigFile
-    )
+    .DESCRIPTION
+    Copies monitoring config xml file from local storage to each server to be monitored
     
-    try{ 
-        #copy XML to c:\drsmonitoring
-        if(Test-path "$pwd\$server.server-DRSMonitoring.xml") {
-            copy "$pwd\$server.server-DRSMonitoring.xml" \\$server.server\c$\DRSMonitoring\$server.server-DRSMonitoring.xml
+    .NOTES
+    Creates a folder on server to store config file if it does not exist. When trying to access the remote server, it will try name first
+    but if that fails, it will try the IP address as well to account for WINS/DNS issues.
+#>    
+    [CMDLETBINDING()]
+    param(
+            # full path to the input csv file
+            [PSObject]$Servers
+        )
+    
+    $msgSource = $MyInvocation.MyCommand.Name
+    $localStorage  = 'C:\MonitoringFiles'
+    
+    Foreach ($server in $Servers) {
+        
+
+        if (Test-Connection $server.server -count 1 -Quiet) {
+            
+            &$log "$($server.server) is accessible by name"
+            $remote = $server.server
+
+        } else {
+            
+            ## try ip before we give up
+            if (Test-Connection $server.ip -count 1 -Quiet) {
+
+                &$log "$($server.server) is NOT accessible by name but is accessible by IP"
+                $remote = $server.ip
+            
+            } else {                
+            
+                Write-Error  "$($server.server) is not accessible by name or IP. Exiting Script!"                
+                exit 1
+            
             }
-        } 
-    catch { 
-            Write-Verbose "Failed copying file to $computer" 
+            
+        }
+
+        &$log "Checking if DRSMonitoring folder exists on remote server"
+        if ( -not ( Test-Path \\$remote\c$\DRSMonitoring) ) {                               
+            
+            try {
+                    &$log  "Creating c:\DRSmonitoring folder on server $($server.server)"
+                    New-Item -Path \\$remote\c$\DRSMonitoring -ItemType Directory -ErrorAction Stop |Out-Null
+                }
+            catch {
+                    Write-Error "Failed to create DRSMonitoring folder on remote server $($server.server). Exiting script!"
+                    exit 1   
             }
+                
+        }
+
+        try { 
+              Copy-Item -Path C:\MonitoringFiles\$($server.server)-*.xml -Destination \\$remote\c$\DRSMonitoring -ErrorAction Stop |out-null
+              &$log "Successfully copied the monitoring file to $($server.server)"
+        }
+        catch {
+              Write-Error "Failed to copy monitoring file to $($server.server). Exiting Script!"
+              exit 1
+       }    
+    }    
+}
+
+#endregion
+
+###  MAIN SCRIPT ###
+
+#1. Create XML File
+
+## $csvFile = Import-Csv $InputFile
+
+#region print verbose messages from anywhere with time and calling function/script name
+$msgSource = $MyInvocation.MyCommand.Name
+$log = {
+
+    param([string]$msg)              
+    Write-Verbose "$(Get-Date -Format 'yyyyMMdd_HHmmss') ${msgsource}: $msg"    
+
 }
 #endregion
 
-#MAIN
+if ($InputFile) {
+    $servers = import-csv $InputFile
+    
+    &$log 'Calling function to create Monitoring Config XML file from supplied CSV for each server'
+    ConvertFrom-CSVToXMLMonitoringFile -Servers $servers
+    
+    &$log 'Calling function to copy local config XML files to remote servers'
+    Copy-ConfigFileToServer -Servers $servers
+}
 
-#1. Create XML File
-Create-DRSMonitoringFile -csvfile $InputFile
+exit
 
 $obj = "" | Select ServerName,RegistyExistButIncorrect,RegistryExistAndCorrect,RegistryDoesNotExist
 
