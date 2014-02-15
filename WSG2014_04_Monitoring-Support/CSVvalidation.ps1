6fa3f009a2958585457e0cfb30f0d0a0fe4ac6aa
Iczer1 <john.mello@outlook.com>
2014-02-15 16:01:56 -0500

    CSV validation
    created bad entries in a CSV list to test the validation

--- WSG2014_04_Monitoring-Support/CSVvalidation.ps1
+++ WSG2014_04_Monitoring-Support/CSVvalidation.ps1
@@ -1,27 +1,37 @@
 ﻿#Used to verfy True/False entries
-$AffNeg = @{
+$VerfiyTable = @{
        "True" = "TRUE"
        "False" = "FALSE"
-       "" = "FALSE"
+       "" = ""
 }
+
+#Used to hold any bad entires found with a description of why they were bad
+$BadEntryList = @()
 
 Write-Verbose "INFO : Pulling in the current calendar list"
 Try {
-       $ServerList = Import-Csv "D:\Users\John\Documents\GitHub\PhillyPosh\WSG2014_04_Monitoring-Support\requirements\servers.csv" -ErrorAction Stop
+       $ServerList = Import-Csv "D:\Users\John\Documents\GitHub\PhillyPosh\WSG2014_04_Monitoring-Support\test\servers_badentries.csv" -ErrorAction Stop
 }
 Catch {
        Write-Warning "Cannot access CSV location"
 	   Exit 1
 }
 
+#Create list of IP's and Servers to test for dupes
+#Not sure if there a cost time wise to using the Automatic foreach in a loop, just to be safe we are going to do it once
+$FullIPList = $ServerList.IP
+$FullServerlist = $ServerList.server
 
 Write-Verbose "ACTION : Verifying server list entries"
 ForEach ($entry in $ServerList) {
-	$BadEntry = $FALSE
-    #Verify that server name is a proper DNS name
-    If ($entry.server -notmatch "^(?!-)[a-zA-Z0-9-]{1,63}(?<!-)$") {$BadEntry ="Blank server name found"; break}
+	$BadEntryReason = ""
+    #Verify that no duplicates server names or IPs exist
+	If (($FullServerlist -match $entry.Server).count -gt 1) {$BadEntryReason += "Duplicate server name in list`n"}
+	If (($FullIPList -match $entry.IP).count -gt 1) {$BadEntryReason += "Duplicate IP in list`n"}
+	#Verify that server name is a proper DNS name
+    If ($entry.server -notmatch "^(?!-)[a-zA-Z0-9-]{1,63}(?<!-)$") {$BadEntryReason += "Server name is not a proper DNS name`n"}
 	#Verify that IP is a proper IP address
-    If ($entry.IP -notmatch "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$") {$BadEntry ="Problem entry Server IP : $($entry.IP)"; break}
+    If ($entry.IP -notmatch "^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$") {$BadEntryReason += "IP address is malformed`n"}
     <#For the following we are going to assume that 
 		any remaning values are true/false
 		any blanks are FALSE
@@ -34,15 +44,17 @@ ForEach ($entry in $ServerList) {
 						ForEach-Object {$_.name}
 	#Verify that they are either true/false or blank
 	$TrueFalseProps | ForEach-Object {
-		If (-not ($AffNeg.containsKey($entry.$_))) {$BadEntry ="Problem entry for $($entry.server) - $_ : $($entry.$_)"; break}
+		If (-not ($VerfiyTable.containsKey($entry.$_))) {$BadEntryReason += "Setting $_ is not True, False, Or blank`n"}
 	}
+	#If one or more bad entries are found add the reason to the entry and the bad entry list
+	If ($BadEntryReason) {$BadEntryList += ($entry | Add-Member -MemberType NoteProperty -Name "BadEntryReason" -Value $BadEntryReason -PassThru)}
 }
  
-If ($BadEntry) {
-       Write-Warning "Bad entries found"
-	   $BadEntry
+If ($BadEntryList) {
+       Write-Warning "Bad Entries found, removing them from the list"
+	   $Serverlist = $Serverlist | Where-Object {$_.Server -notin $BadEntryList.Server}
 }
-Else {
-       Write-Verbose "CSV file contains no errors"
-}
+Else {Write-Verbose "CSV file contains no errors"}
+
+
  
